079f8b19ad063453302bacc1d9c66d6e3614bbb2
JuanMa <juanmafisolis242@gmail.com>
2021-09-04 04:13:17 -0600

    API ready

--- JS/pokeLogic.js
+++ JS/pokeLogic.js
@@ -1,73 +1,83 @@
-var pokeNum = 1;
-var pokeNames = [];
-var pokeData = [];
-var imgPokemon = [];
-var bufferPokemon = [];
-var click = 0;
-var numerCard = 0;
-
-
-document.addEventListener('DOMContentLoaded', () => {
-    fetchData();
-    start();
-})
-
-function home() {
-    pokeNum = 1;
-    click = 0;
-    numerCard = 0;
-    fetchData();
-}
+var pokeNum = 1; // number of pokemon
+var pokeNames = [];// array to save names (size 9)
+var pokeData = [];// array to save only importan data (size 9)
+var imgPokemon = [];// array to save img url (size 9)
+var bufferPokemon = [];// array save all json of pokemon (size 9)
+var click = 0;// clicks
+var numerCard = 0;// number of card
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
+// share API and web
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+    start();
+})
+
+// get data from API
 const fetchData = async () => {
     try {
-        loadData();
-        for (let index = 0; index < 9; index++) {
+
+        loadData(); // disable buttons
+
+        var i = 0;
+
+        // get data onli nine pokemon per turn
+        while (i < 9) {
             const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`);
             const data = await res.json();
-            bufferPokemon[index] = await data;
+            bufferPokemon[i] = await data;
             pokeNum++;
+            i++;
         }
+
+        // pain set name and save importan data 0-9
         for (let index = 0; index < bufferPokemon.length; index++) {
             paintCrad(bufferPokemon[index]);
             numerCard++;
         }
-        numerCard = 0;
-        readyData();
-        managerButton();
+        numerCard = 0;// restar 
+        readyData();// enable button
+        managerButton();// disable button by clicks
+
     } catch (error) {
         console.log(error);
     }
 
 }
 
+// paint pokemon in pokedex table 
 const paintCrad = (pData) => {
-    image = document.getElementById('container' + numerCard.toString());
+    var image = document.getElementById('container' + numerCard.toString());
     image.src = pData.sprites.other.dream_world.front_default;
-    imgPokemon[numerCard] = pData.sprites.other.dream_world.front_default;
-    image.style.width = '100px';
-    image.style.height = '100px';
+    imgPokemon[numerCard] = pData.sprites.other.dream_world.front_default;// save pokemon image
+    image.style.width = '15vh';
+    image.style.height = '15vh';
     setData(pData);
 }
 
+// save only importan data
 const setData = (pData) => {
     var i = 0;
     var BreakException = {};
-    var strgData = "Name : " + pData.name + "<br>" +
+
+    var strgData =
+        "Name : " + pData.name + "<br>" +
         "weight: " + pData.weight + "<br>" +
         "height: " + pData.height + "<br>" +
-        "Moves: <br>";
+        "Moves: ";
+
     try {
         pData.moves.forEach(element => {
-            strgData += element.move.name + ", ";
             if (i == 2) {
+                strgData += element.move.name + "<br>"
                 throw BreakException;
-            }
-            i++;
+            } else {
+                strgData += element.move.name + ",";
+                i++;
+            } 
         });
     } catch (e) {
         if (e !== BreakException) throw e;
@@ -79,73 +89,77 @@ const setData = (pData) => {
         strgData += element.type.name + ", ";
     });
 
-    pokeData[numerCard] = strgData;
-    pokeNames[numerCard] = pData.name;
+    pokeData[numerCard] = strgData; // array to save only importan data (size 9)
+    pokeNames[numerCard] = pData.name;// array to save names (size 9)
     pokeName = pData.name;
     numIndex = 'p' + (numerCard);
-    document.getElementById(numIndex).innerHTML = pokeName;
+    document.getElementById(numIndex).innerHTML = pokeName;//set poke name
 }
 
+// reset
+function home() {
+    pokeNum = 1; // number of pokemon
+    click = 0;// clicks
+    numerCard = 0;// number of card
+    fetchData();
+}
 
-
+// disable buttons
 function loadData() {
     var i = 0;
+    var image;
     document.getElementById("backButton").disabled = true;
     document.getElementById("nextButton").disabled = true;
     while (i < 9) {
         image = document.getElementById(`container${i}`);
         image.src = 'https://media.giphy.com/media/W2LPUUdHkPFNLaWwPZ/giphy.gif?cid=ecf05e47dppbqp02fo7ugqykojmvmo08zbda664qymchtoxd&rid=giphy.gif&ct=s';
-        image.style.width = '100px';
-        image.style.height = '100px';
-
+        image.style.width = '15vh';
+        image.style.height = '15vh';
         document.getElementById(`p${i}`).innerHTML = '. . .';
         document.getElementById(`plus${i}`).disabled = true;
         i++;
     }
 }
 
+// disable buttons
 function readyData() {
     var i = 0;
     document.getElementById("backButton").disabled = false;
-    document.getElementById("nextButton").disabled = false; 
+    document.getElementById("nextButton").disabled = false;
     while (i < 9) {
         document.getElementById(`plus${i}`).disabled = false;
         i++;
     }
 }
 
-
+//enable button
 function start() {
-
     document.getElementById("cardGhostTable").style.display = "none";
     document.getElementById("cardGhostOne").style.display = "none";
     document.getElementById("cardGhostTwo").style.display = "none";
 
     var imagePlus = document.getElementById('plus');
+
     var stringP;
     var imagePlus
-
     for (let index = 0; index < 9; index++) {
         stringP = 'plus' + (index);
         imagePlus = document.getElementById(stringP);
         imagePlus.src = "../Resources/plus.png";
-        imagePlus.style.width = '40px';
-        imagePlus.style.height = 'auto';
     }
-
     click = 0;
 }
 
-
-
-function managerButton(){
+// disables button by clicks
+function managerButton() {
     console.log('-> ', click);
-    if(click==0){
+    if (click == 0) {
         console.log('--> ', click);
         document.getElementById("backButton").disabled = true;
     }
 }
 
+// click funtion in button plus
 function bPlus(pNum) {
     document.getElementById("cardGhostTable").style.display = "block";
     document.getElementById("cardGhostOne").style.display = "block";
@@ -153,48 +167,21 @@ function bPlus(pNum) {
 
     var imageBR = document.getElementById("container00");
     imageBR.src = imgPokemon[pNum];
-    imageBR.style.width = '340px';
-    imageBR.style.height = '290px';
+    imageBR.style.width = '33vh';
+    imageBR.style.height = '33vh';
+
     document.getElementById("msgError").innerHTML = pokeData[pNum];
     document.getElementById("pokeName").innerHTML = pokeNames[pNum];
 }
 
+// click funtion in overlay
 function offData() {
     document.getElementById("cardGhostTable").style.display = "none";
     document.getElementById("cardGhostOne").style.display = "none";
     document.getElementById("cardGhostTwo").style.display = "none";
 }
 
-function sharePokemons(pFalg) {
-    if (pFalg) {
-        fetchData();
-        click++;
-    } else {
-        click--;
-        pokeNum = (click * 9) + 1;
-        if (pokeNum <= 0) {
-            pokeNum = 1
-        }
-        fetchData();
-    }
-}
-
-
-document.addEventListener('DOMContentLoaded', function () {
-    var button = document.getElementById('backButton');
-    button.addEventListener('click', function () {
-        document.getElementById("backButton").disabled = true;
-        click--;
-        pokeNum = (click * 9) + 1;
-        if (pokeNum <= 0) {
-            pokeNum = 1
-        }
-
-        fetchData();
-        document.getElementById("backButton").disabled = false;
-    });
-});
-
+// click funtion in button next 
 document.addEventListener('DOMContentLoaded', function () {
     var button = document.getElementById('nextButton');
     button.addEventListener('click', function () {
@@ -204,3 +191,19 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("nextButton").disabled = false;
     });
 });
+
+// click funtion in button back
+document.addEventListener('DOMContentLoaded', function () {
+    var button = document.getElementById('backButton');
+    button.addEventListener('click', function () {
+        document.getElementById("backButton").disabled = true;
+        click--;
+        pokeNum = (click * 9) + 1;
+        if (pokeNum <= 0) {
+            pokeNum = 1
+        }
+        fetchData();
+        document.getElementById("backButton").disabled = false;
+    });
+});
+
